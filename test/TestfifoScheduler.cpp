3db6137ecaadfadabbee9645fd6775530887f298
unknown <callum.g.pollock@gmail.com>
2019-12-11 17:27:50 +0000

    update5

--- test/TestfifoScheduler.cpp
+++ test/TestfifoScheduler.cpp
@@ -70,25 +70,25 @@ void test_fillReserved(void) {
 	Job* first = new Job(dummy, 1, 16 * 5, 0, 8, 8);
 	Job* second = new Job(dummy, 1, 16 * 4, 0, 8, 8);
 	Job* third = new Job(dummy, 1, 16 * 12, 0, 8, 8);
-	q->insertJob(first);
-	q->insertJob(second);
-	q->insertJob(third);
+	double currentTime = 70; //well before weekend cutoff
+	q->insertJob(first, currentTime);
+	q->insertJob(second, currentTime);
+	q->insertJob(third, currentTime);
 	std::vector<Job*> results; //our return vector
 	int state = 2; //start of in a state where there is no need to check time
-	double currentTime = 70; //well before weekend cutoff
 	results = fifo.fillReserved(running, runningTotal, q, state, 3, WEEKENDCUTOFF, currentTime, medMin, currentTime);
 	TEST_ASSERT(results.size() == 2); //should only have taken first two jobs
-	TEST_ASSERT(q.size()==1); //still one guy waiting in line
+	TEST_ASSERT(q.getJobsInQueue().size()==1); //still one guy waiting in line
 	TEST_ASSERT_EQUAL_INT(39, running); //should have increased nodes running to 39
 	TEST_ASSERT_EQUAL_INT(39, runningTotal); //should have increased nodes running to 39
 	running =30; runningTotal=30; //reset these to run new tests here
 	state=3; //now needs to check times
 	currentTime= 98; //6 hours before weekend cut off
 	Job* fourth = new Job(dummy, 1, 16 * 10, 0, 4, 4);
-	q->insertJob(fourth);
+	q->insertJob(fourth, currentTime);
 	results = fifo.fillReserved(running, runningTotal, q, state, 3, WEEKENDCUTOFF, currentTime, medMin, currentTime);
 	TEST_ASSERT(results.size() == 1); //should have just taken the one job this time
-	TEST_ASSERT(q.size()==1); //still one guy waiting in line
+	TEST_ASSERT(q->getJobsInQueue().size()==1); //still one guy waiting in line
 	TEST_ASSERT_EQUAL_INT(40, running); //should have increased nodes running to 36
 	TEST_ASSERT_EQUAL_INT(40, runningTotal); //should have increased nodes running to 36
 	
